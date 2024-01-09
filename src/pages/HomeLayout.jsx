7d29f5f797639e3f7b6043147298850e6b24e116
유애나 <77397206+Teerapat28@users.noreply.github.com>
2024-01-09 17:05:21 +0700

    Header / Navbar / Navlinks

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Header,Navbar } from "../components";
 
 const HomeLayout = () => {
   return (
     <>
-      <nav>
-        <span className="text-4xl text-primary">Comfy</span>
-      </nav>
-      <Outlet />
+      <Header/>
+      <Navbar/>
+      <section className="align-element py-20">
+        <Outlet />
+      </section>
     </>
   );
 };
