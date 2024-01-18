424fee0558ffbdba61a7b9aaad2c2a740cbd6337
유애나 <77397206+Teerapat28@users.noreply.github.com>
2024-01-18 18:47:49 +0700

    Pagination

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from "react";
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
 
 const PaginationContainer = () => {
-  return (
-    <div>PaginationContainer</div>
-  )
-}
+  const { meta } = useLoaderData();
+  const { pageCount, page } = meta.pagination;
 
-export default PaginationContainer
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    return index + 1;
+  });
+
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+    // console.log(search);
+    // console.log(pathname);
+    // console.log(pageNumber);
+  };
+
+  if (pageCount < 2) {
+    return null;
+  }
+  return (
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button
+          className="btn btn-xs sm:btn-md join-item"
+          onClick={() => {
+            let prevPage = page - 1;
+            if (prevPage < 1) {
+              prevPage = pageCount;
+            }
+            handlePageChange(prevPage);
+          }}
+        >
+          Prev
+        </button>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
+              className={`btn btn-xs sm:btn-md border-none join-item ${
+                pageNumber === page ? "bg-base-300 border-base-300" : ""
+              }`}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+        <button
+          className="btn btn-xs sm:btn-md join-item"
+          onClick={() => {
+            let nextPage = page + 1;
+            if (nextPage > pageCount) {
+              nextPage = 1;
+            }
+            handlePageChange(nextPage);
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PaginationContainer;
